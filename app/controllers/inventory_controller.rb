2f95d08d283368102d023fb22b8a945a2dfef38a
AbhishekSaha <as1695@scarletmail.rutgers.edu>
2015-12-03 23:05:35 -0500

    Backend complete

--- app/controllers/inventory_controller.rb
+++ app/controllers/inventory_controller.rb
@@ -1,13 +1,25 @@
 class InventoryController < ApplicationController
 
+  require 'json'
   layout 'application'
+
+  def lookupVIN
+
+    lops = Caroption.where("VIN = "  + "'" + params['VIN']+ "'")
+
+    rez = {"near" => JSON.parse(lops.to_json)}
+
+
+    respond_to do |format|
+      format.json { render json: rez }  # respond with the created JSON object
+    end
+  end
+
+
 
   def client
     @inventorys = Inventory.all
     if params['commit'].present?
-      puts("Got this far")
-
-      query = "Select i.* from INVENTORY i, options o"
 
       inventory = {}
       inventory[:make] = params['make']
@@ -22,7 +34,6 @@ class InventoryController < ApplicationController
       caropts[:DVD] = "Yes" unless params['dvd'].blank?
 
       @VINS = Inventory.all.where(:INVENTORY => inventory).select("VIN")
-      puts("Crap")
 
       @inventorys = Inventory.joins("INNER JOIN caroptions ON inventories.VIN = caroptions.VIN")
                         .where(:inventories => inventory, :caroptions => caropts)
