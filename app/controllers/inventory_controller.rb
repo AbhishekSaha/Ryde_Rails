938a19c872c682d4ddfbab910ad2987d4542d74a
AbhishekSaha <as1695@scarletmail.rutgers.edu>
2015-12-05 19:03:10 -0500

    Updated Distance

--- app/controllers/inventory_controller.rb
+++ app/controllers/inventory_controller.rb
@@ -50,10 +50,11 @@ class InventoryController < ApplicationController
   end
 
   def client
+    @div = "#page-top"
     @inventorys = Inventory.order("DISTANCE")
 
     if params['commit'].present?
-
+      @div = "#about"
       inventory = {}
       inventory[:make] = params['make'] unless params['make'].blank?
       inventory[:type] = params['type'] unless params['type'].blank?
@@ -68,8 +69,13 @@ class InventoryController < ApplicationController
 
       @VINS = Inventory.all.where(:INVENTORY => inventory).select("VIN")
 
-      if caropts.blank?
+      if caropts.blank? && invetonry.blank?
+        @inventorys = Inventory.order("DISTANCE")
+      elsif caropts.blank?
         @inventorys = Inventory.where(:inventories => inventory).order("Distance")
+      elsif inventory.blank?
+        @inventorys = Inventory.joins("INNER JOIN caroptions ON inventories.VIN = caroptions.VIN")
+                          .where(:caroptions => caropts)
       else
         @inventorys = Inventory.joins("INNER JOIN caroptions ON inventories.VIN = caroptions.VIN")
                           .where(:inventories => inventory, :caroptions => caropts)
